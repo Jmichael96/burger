64adcc46a71309c04ef2b749f4cdd21ce7063874
Jeffreyvh <jeffreyvh@ymail.com>
2018-11-19 18:26:56 -0600

    finished product but needs styling

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Jameson Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mcdouble Burger');
+INSERT INTO burgers (burger_name) VALUES ('Tripple Decker Burger');
+INSERT INTO burgers (burger_name) VALUES ('Amazon Burger');
--- server.js
+++ server.js
@@ -1,1 +1,38 @@
-var express = require('express');   
+// DEPENDENCIES
+// ==============================================
+var express = require('express');
+var exphbs = require('express-handlebars');
+var mysql = require('mysql');
+
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+
+// SETUP EXPRESS SERVER
+// ==============================================
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+// Tells app to use handlebars to create the layout
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// Import and use router from controller.js file
+var router = require('./controllers/burgers_controller');
+app.use('/', router);
+
+
+// DEFINE PORT AND START SERVER LISTEN
+// ==============================================
+var PORT = 8080;
+app.listen(process.env.PORT || PORT);
+console.log("Local host:" + PORT);
