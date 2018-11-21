5f34bd0b8324f1e234f554f73a34db5234231259
Jeffreyvh <jeffreyvh@ymail.com>
2018-11-20 20:07:17 -0600

    updated

--- config/connections.js
+++ config/connections.js
@@ -13,7 +13,7 @@ if (process.env.JAWSDB_URL) {
 		database: 'burgers_db'
 	});
 };
-connection.connect(function (err) {
+connection.connect(function(err) {
 	if (err) {
 		console.error('error connecting: ' + err.stack);
 		return;
